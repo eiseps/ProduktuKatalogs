267799715f7ff505e0bfde9449dd2426171b4a9f
eiseps <arnis.stucka@gmail.com>
2025-09-14 22:29:00 +0300

    Update Form1.cs ar joku

--- ProduktuKatalogs/Form1.cs
+++ ProduktuKatalogs/Form1.cs
@@ -23,7 +23,7 @@
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Kļūda, pievienojot grāmatu: {ex.Message}", "Kļūda", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Kļūda, pievienojot grāmatu hey straada: {ex.Message}", "Kļūda", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
